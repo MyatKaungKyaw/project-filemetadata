20e5eeeb0f6fbd43540df3f6f9a3222b36ae63ab
LuMike <LuMike@users.noreply.github.com>
2022-04-21 21:19:49 +0630

    add file analyse features

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
-
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.use((req,res,next)=>{
+  console.log(`${req.method}, ${req.path}, ${req.ip}`);
+  next();
 });
 
 
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
 
+
+app.route('/api/fileanalyse')
+.post(upload.single('upfile'),(req,res) =>{
+  const file = req.file;
+  res.json({
+    name : file.originalname,
+    type : file.mimetype,
+    size : file.size,
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
